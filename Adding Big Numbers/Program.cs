9d428991fb9a2afe7a913482680f4a2e4ef16145
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-07-28 11:56:57 +0300

    Добавьте файлы проекта.

---
+++ Adding Big Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+public class Kata
+{
+    public static void Main()
+    {
+        // Test
+        var t = Add("123", "321");
+        // ...should return "444"
+    }
+
+    public static string Add(string a, string b)
+    {
+        BigInteger A = BigInteger.Parse(a);
+        BigInteger B = BigInteger.Parse(b);
+
+        return (A + B).ToString();
+    }
+}
